8f8528a86ce6c3f31b86a76e2ba29979d2629910
Regina-Ma <regina.martinkute@gmail.com>
2020-08-26 16:51:32 +0300

    category model updated

--- models/Car/category.js
+++ models/Car/category.js
@@ -2,6 +2,41 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+const partNamesSchema = new Schema({
+  partName: {
+    type: String,
+    required: true,
+  },
+  translations: {
+    lt: {
+      type: String,
+      required: true,
+    },
+    en: {
+      type: String,
+      required: true,
+    },
+  },
+});
+
+const subcategorySchema = new Schema({
+  subcategoryName: {
+    type: String,
+    required: true,
+  },
+  translations: {
+    lt: {
+      type: String,
+      required: true,
+    },
+    en: {
+      type: String,
+      required: true,
+    },
+  },
+  partNames: [partNamesSchema],
+});
+
 // kategorijos schema
 const categorySchema = new Schema({
   categoryName: {
@@ -18,48 +53,11 @@ const categorySchema = new Schema({
       required: true,
     },
   },
-
   iconUrl: {
     type: String,
     required: true,
   },
-  subcategories: [
-    {
-      subcategoryName: {
-        type: String,
-        required: true,
-      },
-      translations: {
-        lt: {
-          type: String,
-          required: true,
-        },
-        en: {
-          type: String,
-          required: true,
-        },
-      },
-
-      partNames: [
-        {
-          partName: {
-            type: String,
-            required: true,
-          },
-          translations: {
-            lt: {
-              type: String,
-              required: true,
-            },
-            en: {
-              type: String,
-              required: true,
-            },
-          },
-        },
-      ],
-    },
-  ],
+  subcategories: [subcategorySchema],
 });
 
 module.exports = mongoose.model("Category", categorySchema);
